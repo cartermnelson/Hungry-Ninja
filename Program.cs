bccb2007fcbe85ea061bd8127afc9e0f6c30bf88
Carter Nelson <carternelson05@gmail.com>
2019-10-16 10:18:11 -0700

    Created Food, Buffet, and Ninja classes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Hungry_Ninja
 {
@@ -7,6 +8,46 @@ namespace Hungry_Ninja
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+    class Food
+    {
+        public string Name;
+        public int Calories;
+        // Foods can be Spicy and/or Sweet
+        public bool IsSpicy;
+        public bool IsSweet;
+        // add a constructor that takes in all four parameters: Name, Calories, IsSpicy, IsSweet
+    }
+    class Buffet
+    {
+        public List<Food> Menu;
+
+        //constructor
+        public Buffet()
+        {
+            Menu = new List<Food>()
+        {
+            new Food("Example", 1000, false, false)
+        };
+        }
+
+        public Food Serve()
+        {
+        }
+    }
+    class Ninja
+    {
+        private int calorieIntake;
+        public List<Food> FoodHistory;
+
+        // add a constructor
+
+        // add a public "getter" property called "IsFull"
+
+        // build out the Eat method
+        public void Eat(Food item)
+        {
         }
     }
 }
