636cf0c15d0d45fbba895be1deeb02c95a41cba5
Carter Nelson <carternelson05@gmail.com>
2019-10-16 15:35:08 -0700

    Added constructors and methods

--- Program.cs
+++ Program.cs
@@ -18,6 +18,13 @@ namespace Hungry_Ninja
         public bool IsSpicy;
         public bool IsSweet;
         // add a constructor that takes in all four parameters: Name, Calories, IsSpicy, IsSweet
+        public Food(string nam, int cal, bool spi, bool swe)
+        {
+            Name = nam;
+            Calories = cal;
+            IsSpicy = spi;
+            IsSweet = swe;
+        }
     }
     class Buffet
     {
@@ -26,14 +33,20 @@ namespace Hungry_Ninja
         //constructor
         public Buffet()
         {
-            Menu = new List<Food>()
-        {
-            new Food("Example", 1000, false, false)
-        };
+            Menu = new List<Food>();
+            Menu.Add(new Food("Pizza", 700, false, false));
+            Menu.Add(new Food("Pasta", 700, false, false));
+            Menu.Add(new Food("Burrito", 700, false, true));
+            Menu.Add(new Food("Taco", 300, false, true));
+            Menu.Add(new Food("Burger", 700, false, false));
+            Menu.Add(new Food("Hot Dog", 500, false, false));
+            Menu.Add(new Food("French Fries", 300, false, false));
         }
 
-        public Food Serve()
+        public Food Serve(Food food)
         {
+            Random rand = new Random();
+            return Menu[rand.Next(Menu.Count)];
         }
     }
     class Ninja
@@ -42,12 +55,33 @@ namespace Hungry_Ninja
         public List<Food> FoodHistory;
 
         // add a constructor
+        public Ninja()
+        {
+            calorieIntake = 0;
+            FoodHistory = new List<Food>();
+        }
 
         // add a public "getter" property called "IsFull"
-
+        public bool IsFull
+        {
+            get
+            {
+                return IsFull;
+            }
+        }
         // build out the Eat method
         public void Eat(Food item)
         {
+            if (IsFull == false)
+            {
+                calorieIntake += item.Calories;
+                FoodHistory.Add(item);
+                Console.WriteLine($"Ninja ate {item.Name} which has {item.Calories} calories. Sweet: {item.IsSweet}; Spicy: {item.IsSpicy}");
+            }
+            else
+            {
+                Console.WriteLine("Ninja is full and cannot eat anymore");
+            }
         }
     }
 }
